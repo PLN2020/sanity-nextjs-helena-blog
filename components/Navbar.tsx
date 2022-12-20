e5d92d193d576286d22d534e0bba0817fe130dee
Patrick Lam Nguyen <patricklamnguyen@gmail.com>
2022-12-19 21:25:50 -0500

    Group routing and started the Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from 'react-icons/fa'
+
+export default function Navbar() {
+    return (
+        <nav className="sticky flex justify-between items-center top-0 h-[10vh] bg-gray-100 px-10">
+            <div>
+                <Link href='/'>Helena's Blog</Link>
+            </div>
+            
+            <div className="flex justify-center items-center">
+                <div className="flex space-x-4 mr-10">
+                    <Link href='/about'>About</Link>
+                    <Link href='/portfolio'>Portfolio</Link>
+                    <Link href='/blog'>Blog</Link>
+                    <Link href='/contact'>Contact</Link>  
+                </div>
+
+                <div className="border-l-2 border-gray-600 h-8" />
+
+                <div className="flex space-x-4 ml-10">
+                    <FaTwitter />
+                    <FaInstagram />
+                    <FaFacebook />
+                    <FaYoutube />
+                </div>
+            </div>
+        </nav>
+    )
+}
